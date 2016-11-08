8cb296e26deba9458377b44af54adf426d502bab
Essem <essemfly@gmail.com>
2016-11-08 23:29:01 +0900

    Refactor to use internal response methods in views

--- hoover/views.py
+++ hoover/views.py
@@ -1,58 +1,43 @@
-from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
-from hoover.models import Hoover, Review
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from hoover.models import Hoover
 from hoover.serializers import HooverSerializer
 
-class JSONResponse(HttpResponse):
-    def __init__(self, data, **kwargs):
-        content = JSONRenderer().render(data)
-        kwargs['content_type'] = 'application/json'
-        super(JSONResponse, self).__init__(content, **kwargs)
 
-
-@csrf_exempt
+@api_view(['GET', 'POST'])
 def hoover_list(request):
-    """
-    List all code hoovers, or create a new hoover.
-    """
     if request.method == 'GET':
         hoovers = Hoover.objects.all()
         serializer = HooverSerializer(hoovers, many=True)
-        return JSONResponse(serializer.data)
+        return Response(serializer.data)
 
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = HooverSerializer(data=data)
+        serializer = HooverSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JSONResponse(serializer.data, status=201)
-        return JSONResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@csrf_exempt
+@api_view(['GET', 'PUT', 'DELETE'])
 def hoover_detail(request, pk):
-    """
-    Retrieve, update or delete a code hoover.
-    """
     try:
         hoover = Hoover.objects.get(pk=pk)
     except Hoover.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
         serializer = HooverSerializer(hoover)
-        return JSONResponse(serializer.data)
+        return Response(serializer.data)
 
     elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = HooverSerializer(hoover, data=data)
+        serializer = HooverSerializer(hoover, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JSONResponse(serializer.data)
-        return JSONResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE':
         hoover.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
