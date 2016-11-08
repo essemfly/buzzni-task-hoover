e5d58341332798117777115b355bbb298c78c910
Essem <essemfly@gmail.com>
2016-11-08 23:43:25 +0900

    Change to class based Views

--- hoover/urls.py
+++ hoover/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
 from hoover import views
 
 urlpatterns = [
-    url(r'^hoover/$', views.hoover_list),
-    url(r'^hoover/(?P<pk>[0-9]+)/$', views.hoover_detail),
+    url(r'^hoover/$', views.HooverList.as_view()),
+    url(r'^hoover/(?P<pk>[0-9]+)/$', views.HooverDetail.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- hoover/views.py
+++ hoover/views.py
@@ -1,43 +1,45 @@
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.http import Http404
 from hoover.models import Hoover
 from hoover.serializers import HooverSerializer
 
 
-@api_view(['GET', 'POST'])
-def hoover_list(request):
-    if request.method == 'GET':
+class HooverList(APIView):
+    def get(self, request, format=None):
         hoovers = Hoover.objects.all()
         serializer = HooverSerializer(hoovers, many=True)
         return Response(serializer.data)
 
-    elif request.method == 'POST':
+    def post(self, request, format=None):
         serializer = HooverSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+class HooverDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Hoover.objects.get(pk=pk)
+        except Hoover.DoesNotExist:
+            return Http404
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def hoover_detail(request, pk):
-    try:
-        hoover = Hoover.objects.get(pk=pk)
-    except Hoover.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'GET':
+    def get(self, request, pk, format=None):
+        hoover = self.get_object(pk)
         serializer = HooverSerializer(hoover)
         return Response(serializer.data)
 
-    elif request.method == 'PUT':
+    def put(self, request, pk, format=None):
+        hoover = self.get_object(pk)
         serializer = HooverSerializer(hoover, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == 'DELETE':
+    def delete(self, request, pk, format=None):
+        hoover = self.get_object(pk)
         hoover.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
