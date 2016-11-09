afe758b7900ead4b5da63c20be937cbcadb6fd3b
Essem <essemfly@gmail.com>
2016-11-09 19:59:08 +0900

    Add hoover search view

--- hoover/views.py
+++ hoover/views.py
@@ -6,6 +6,14 @@ from hoover.models import Hoover
 from hoover.serializers import HooverSerializer
 
 
+class HooverSearch(APIView):
+    def get(self, request, format=None):
+        keyword = request.query_params.get('keyword', None)
+        hoovers = Hoover.objects.all().order_by('-avg_rating')[:5]
+        serializer = HooverSerializer(hoovers, many=True)
+        return Response(serializer.data)
+
+
 class HooverList(APIView):
     def get(self, request, format=None):
         hoovers = Hoover.objects.all()
@@ -18,6 +26,7 @@ class HooverList(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class HooverDetail(APIView):
     def get_object(self, pk):
