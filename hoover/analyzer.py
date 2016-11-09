7af37e6a79827378505207be24badf30b4cea9f4
Essem <essemfly@gmail.com>
2016-11-10 03:17:13 +0900

    Refactor analyzer function

--- hoover/analyzer.py
+++ hoover/analyzer.py
@@ -15,17 +15,20 @@ def get_keyword(doc):
 
 
 def get_recommended_hoover(keywords):
-    pos_tagger = Twitter()
     hoover_ids = Hoover.objects.values_list('id', flat=True)
     hoover_scores = {}
     for hoover_id in hoover_ids:
-        hoover_score = 0
-        reviews = Review.objects.filter(product_id_id=hoover_id)
-        for review in reviews:
-            review_nouns = set(pos_tagger.nouns(review.content))
-            for keyword in keywords:
-                if keyword in review_nouns:
-                    hoover_score += review.rating-3
-        hoover_scores[hoover_id] = hoover_score
+        hoover_scores[hoover_id] = get_hoover_score(hoover_id, keywords)
     return hoover_scores
 
+
+def get_hoover_score(hoover_id, keywords):
+    hoover_score = 0
+    pos_tagger = Twitter()
+    reviews = Review.objects.filter(product_id_id=hoover_id)
+    for review in reviews:
+        review_nouns = set(pos_tagger.nouns(review.content))
+        for keyword in keywords:
+            if keyword in review_nouns:
+                hoover_score += review.rating - 3
+    return hoover_score
