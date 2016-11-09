e538c558cf83eea2ffc0371f0dec51e767957c77
Essem <essemfly@gmail.com>
2016-11-10 03:16:38 +0900

    Divide url for searching hoovers

--- hoover/urls.py
+++ hoover/urls.py
@@ -3,7 +3,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from hoover import views
 
 urlpatterns = [
-    url(r'^', views.HooverSearch.as_view()),
+    url(r'^search/$', views.HooverSearch.as_view()),
     url(r'^hoover/$', views.HooverList.as_view()),
     url(r'^hoover/(?P<pk>[0-9]+)/$', views.HooverDetail.as_view()),
 ]
