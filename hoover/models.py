9c1629ee2cf5cd3efd823c8f3859fedfa8d5d89d
Essem <essemfly@gmail.com>
2016-11-10 11:31:12 +0900

    Attach review objects to hoover objects

--- hoover/models.py
+++ hoover/models.py
@@ -8,11 +8,16 @@ class Hoover(models.Model):
     basic_info = models.CharField(max_length=400)
     avg_rating = models.FloatField(default=0)
 
+    def get_review(self):
+        return self.review.all()[:5]
+
 
 class Review(models.Model):
-    product_id = models.ForeignKey(Hoover, on_delete=models.CASCADE)
+    product_id = models.ForeignKey(Hoover, related_name='review', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     content = models.TextField()
     rating = models.IntegerField(default=0)
     pub_date = models.DateTimeField()
 
+    class Meta:
+        ordering = ['-rating']
--- hoover/serializers.py
+++ hoover/serializers.py
@@ -2,7 +2,15 @@ from rest_framework import serializers
 from hoover.models import Hoover, Review
 
 
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = ('id', 'title', 'content', 'rating')
+
+
 class HooverSerializer(serializers.ModelSerializer):
+    review = ReviewSerializer(many=True, read_only=True, source='get_review')
+
     class Meta:
         model = Hoover
-        fields = ('id', 'nvmid', 'name', 'price', 'avg_rating')
+        fields = ('id', 'nvmid', 'name', 'price', 'avg_rating', 'review')
